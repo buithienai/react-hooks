bb3e0858b258e55ee36502892a839289fd36a62c
ai.bt@techchain.vn <ai.bt@techchain.vn>
2019-12-20 15:29:10 +0700

    Config env

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function init(initialCount) {
+	return { count: initialCount };
 }
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'increment':
+			return { count: state.count + 1 };
+		case 'decrement':
+			return { count: state.count - 1 };
+		case 'reset':
+			return init(1);
+		default:
+			throw new Error();
+	}
+}
+
+function App({ initialCount }) {
+	const [state, dispatch] = useReducer(reducer, 1, init);
+	const [name, setName] = useState('');
+	const [errorName, setErrorName] = useState(false);
+
+	const handleChangeName = (value) => {
+		setName(value);
+		setErrorName(false);
+	}
+
+	const handleSubmit = () => {
+		if (name.length === 0) {
+			setErrorName(true);
+			return;
+		}
+
+		console.log(name);
+	}
+
+	return (
+		<>
+			Count: {state.count}
+			<button
+				onClick={() => dispatch({ type: 'reset', payload: initialCount })}>
+				Reset
+			</button>
+			<button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+			<button onClick={() => dispatch({ type: 'increment' })}>+</button>
+
+			<div className={errorName ? 'has-error' : ''}>
+				<input type="text" value={name} onChange={(e) => handleChangeName(e.target.value)} />
+			</div>
+			<button
+				onClick={handleSubmit}
+			>Submit</button>
+		</>
+	);
+}
+
 
 export default App;
---
+++ src/configs/env/index.js
@@ -0,0 +1,9 @@
+const app = {
+    env: {
+        localhost: 'localhost',
+        production: 'production',
+        development: 'development'
+    }
+}
+
+export default Object.assign(app)
